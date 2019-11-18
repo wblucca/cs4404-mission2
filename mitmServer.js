6a8d492a15d98c684771cfc9068fbad9f24731b5
wblucca <wblucca@wpi.edu>
2019-11-18 16:42:33 -0500

    Almost fixed UDP message sending

--- mitmServer.js
+++ mitmServer.js
@@ -14,17 +14,17 @@ const bombastDNS = '10.4.18.1';
 const PORT = 53;
 
 // List of other ISPs to redirect
-const otherISPs = ["berizon.com", "atb.com"];
+const otherISPs = ['berizon', 'atb'];
 // Bombast's domain name
-const bombastDomain = 'bombast.com';
+const bombastDomain = 'bombast';
 
 // Callback for handling messages
 server.on('message', function(msg, rinfo) {
     console.log('Message from ' + rinfo.address + ':' + rinfo.port);
     
     // Check for other ISPs
-    for (const otherISP in otherISPs) {
-        if (msg.toString().includes(otherISP)) {
+    for (var i = 0; i < otherISPs.length; i++) {
+        if (msg.toString().includes(otherISPs[i])) {
             // Send a modified request to DNS server that asks for bombast.com
             forwardModifiedRequest(msg);
         }
@@ -52,19 +52,22 @@ const forwardRequest = function(msg) {
     // Start UDP message sending client
     const client = dgram.createSocket('udp4');
 
+    console.log('Forwarding message to ' + bombastDNS + ':' + PORT + ':\n' + msg.toString());
+
     // Send the message, close on completion
-    client.send(msg, PORT, bombastDNS, function(err) {
+    client.send(msg, 0, msg.length, PORT, bombastDNS, function(err) {
         client.close();
     });
-
-    console.log('Forwarded query to ' + bombastDNS + ':' + PORT);
 }
 
 const forwardModifiedRequest = function(msg) {
-    // Replace other ISP domain with bombast.com
-    for (const otherISP in otherISPs) {
-        msg = msg.replace(otherISP, bombastDomain);
-        console.log(otherISP + ' changed to ' + bombastDomain + ' in request');
+    // Replace other ISP domain with bombast
+    for (var i = 0; i < otherISPs.length; i++) {
+        let n = msg.indexOf(otherISPs[i]);
+        if (n != -1) {
+            msg = msg.slice(0, n) + bombastDomain + msg.slice(n + bombastDomain.length);
+            console.log(otherISPs[i] + ' changed to ' + bombastDomain + ' in request');
+        }
     }
 
     // Forward the bombast.com request
