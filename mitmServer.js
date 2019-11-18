755e5376e9f2a44dcfe2e2d41ba81534cc4904fa
wblucca <wblucca@wpi.edu>
2019-11-18 15:53:18 -0500

    Fix bugs, add some constants, loglines

--- mitmServer.js
+++ mitmServer.js
@@ -10,9 +10,13 @@ const server = dgram.createSocket('udp4');
 
 // Address of Bombast's DNS server to forward to
 const bombastDNS = '10.4.18.1';
+// DNS port number
+const PORT = 53;
 
-// List of other ISPs to block
+// List of other ISPs to redirect
 const otherISPs = ["berizon.com", "atb.com"];
+// Bombast's domain name
+const bombastDomain = 'bombast.com';
 
 // Callback for handling messages
 server.on('message', function(msg, rinfo) {
@@ -42,22 +46,25 @@ server.on('listening', function() {
 });
 
 // Start the server listening for UDP messages (DNS queries)
-server.bind(53);
+server.bind(PORT);
 
 const forwardRequest = function(msg) {
     // Start UDP message sending client
     const client = dgram.createSocket('udp4');
 
     // Send the message, close on completion
-    client.send(message, 52, bombastDNS, function(err) {
+    client.send(msg, PORT, bombastDNS, function(err) {
         client.close();
     });
+
+    console.log('Forwarded query to ' + bombastDNS + ':' + PORT);
 }
 
 const forwardModifiedRequest = function(msg) {
     // Replace other ISP domain with bombast.com
     for (const otherISP in otherISPs) {
-        msg = msg.replace(otherISP, 'bombast.com');
+        msg = msg.replace(otherISP, bombastDomain);
+        console.log(otherISP + ' changed to ' + bombastDomain + ' in request');
     }
 
     // Forward the bombast.com request
