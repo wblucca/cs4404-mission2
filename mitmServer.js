b5f659e234ed49480d59feb6f4e76c23a89f1dee
wblucca <wblucca@wpi.edu>
2019-11-19 11:17:13 -0500

    Listen on 3000 and forward to 10.4.18.3:53

--- mitmServer.js
+++ mitmServer.js
@@ -8,15 +8,23 @@ const dgram = require('dgram');
 // Create the UDP server
 const server = dgram.createSocket('udp4');
 
-// Address of Bombast's DNS server to forward to
-const bombastDNS = '10.4.18.1';
+// DNS query uses char code 0x03 for periods in domain name
+const DOT = String.fromCharCode(0x03);
+
+// Port for this meddler server to listen on
+const MEDDLER_PORT = 3000;
 // DNS port number
-const PORT = 53;
+const DNS_PORT = 53;
+// Address of root DNS server to forward to
+const ROOT_IP = '10.4.18.3';
 
 // List of other ISPs to redirect
-const otherISPs = ['berizon', 'atb'];
+const otherISPs = [
+    'berizon' + DOT + 'com',
+    'atb' + DOT + 'com'
+];
 // Bombast's domain name
-const bombastDomain = 'bombast';
+const bombastDomain = 'bombast' + DOT + 'com';
 
 // Callback for handling messages
 server.on('message', function(msg, rinfo) {
@@ -25,12 +33,13 @@ server.on('message', function(msg, rinfo) {
     // Check for other ISPs
     for (var i = 0; i < otherISPs.length; i++) {
         if (msg.toString().includes(otherISPs[i])) {
-            // Send a modified request to DNS server that asks for bombast.com
+            // Send a modified request to root DNS server that asks for bombast.com
             forwardModifiedRequest(msg);
+            return;
         }
     }
 
-    // Forward to DNS
+    // Forward to root DNS
     forwardRequest(msg);
 });
 
@@ -46,16 +55,16 @@ server.on('listening', function() {
 });
 
 // Start the server listening for UDP messages (DNS queries)
-server.bind(PORT);
+server.bind(MEDDLER_PORT);
 
 const forwardRequest = function(msg) {
     // Start UDP message sending client
     const client = dgram.createSocket('udp4');
 
-    console.log('Forwarding message to ' + bombastDNS + ':' + PORT + ':\n' + msg.toString());
+    console.log('Forwarding message to ' + ROOT_IP + ':' + DNS_PORT + ':\n' + msg.toString());
 
     // Send the message, close on completion
-    client.send(msg, 0, msg.length, PORT, bombastDNS, function(err) {
+    client.send(msg, DNS_PORT, ROOT_IP, function(err) {
         client.close();
     });
 }
